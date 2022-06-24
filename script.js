e3bded967cbe185033a68a57600cf27cf5ce366a
David Zonana <davidzn2@gmail.com>
2022-06-24 12:59:30 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,40 @@
+console.log('Me imprimen desde el archivo externo!!')
+
+var edad = 27
+var pi = 3.1416
+var negativeNumber = -9812
+
+var nombre = "David "
+var apellido = "Zonana"
+
+var booleanVar = false
+var nombreCompleto = nombre + apellido
+console.log('La edad de ' + nombreCompleto + ' es: ' + edad)
+
+var edadUser = prompt('Cual es tu eedad brother??')
+console.log('La edad del usuario es:', edadUser)
+
+var nombreUser = prompt('Cual es tu nombre?')
+console.log('Hola', nombreUser)
+
+var nulo = null
+var noTengoNada
+console.log(typeof(x))
+
+
+// operaciones
+
+var x = 3
+var y = 27
+var suma = x + y
+var resta = x - y
+var multiplicacion = x * 6
+var division = y / x
+var modulo = x % y //Residuo de una division
+var potencia = x ** 2
+console.log("Suma:",suma)
+console.log("Resta:",resta)
+console.log('Multiplicacion:',multiplicacion)
+console.log('Division:',division)
+console.log('Residuo de la division',modulo)
+console.log('Potencia:', potencia)
